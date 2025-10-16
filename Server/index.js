50a8b0aa34b58f2f47c9522d7a9aee8a1fc4f701
sahanr <sahanr@infyinnos.com>
2025-10-16 17:50:18 +0530

    deploying code new

--- Server/index.js
+++ Server/index.js
@@ -17,6 +17,8 @@ const PORT = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/weather', weatherRouter);
+
 // Serve frontend
 
 app.use(express.static(path.join(process.cwd(), "public")));
@@ -25,10 +27,6 @@ app.get(/.*/, (req, res) => {
   res.sendFile(path.join(process.cwd(), "public", "index.html"));
 });
 
-
-
-
-app.use('/api/weather', weatherRouter);
 
 app.listen(
   PORT, 
